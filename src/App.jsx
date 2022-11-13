df4bbb18bd8e13fd76a24663bca3fba21afc3eb4
ahmed elamin <ahmedselamin@gmail.com>
2022-11-13 09:24:51 +0200

    initial setup

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const App = () => {
+  return (
+    <div className="app">
+      <h1>QR Code Generator</h1>
+      <input type="text" placeholder="Enter something.." />
+      <button>Generate</button>
+    </div>
+  );
+};
+
+export default App;
